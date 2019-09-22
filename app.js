5c8064bbeb92172553cbd512a0360b2333728118
Tej Kahlon <tejpartapsingh91@gmail.com>
2019-09-22 11:36:38 -0400

    Revise server

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import express from 'express';
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.get(`/`, (req, res) => res.send(JSON.stringify({ Hello: `World`})));
+app.use(express.static(path.join(__dirname, '/build')));
+
+app.get(`/`, (req, res) => res.sendFile(path.join(__dirname+'/build/index.html')));
 
 app.listen(port, () => console.log(`Example app listening on port !`));
