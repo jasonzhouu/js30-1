32f28a7bf8b72d9cf522ead2b2077f6b60331e8d
Tej Kahlon <tejpartapsingh91@gmail.com>
2019-09-22 11:30:10 -0400

    Add express

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.get(`/`, (req, res) => res.send(JSON.stringify({ Hello: `World`})));
+
+app.listen(port, () => console.log(`Example app listening on port !`));
