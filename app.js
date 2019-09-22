2c7ac2a7cd1afe9854a9a5721383e91fce51637c
Tej Kahlon <tejpartapsingh91@gmail.com>
2019-09-22 11:40:46 -0400

    Revise changes

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import path from 'path';
 
-var port = process.env.PORT || 3000;
-var app = express();
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(__dirname + '/build'));
 
-app.get(`/`, (req, res) => res.sendFile(path.join(__dirname+'/build/index.html')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 
-app.listen(port, () => console.log(`Example app listening on port !`));
+app.listen(port);
